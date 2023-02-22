b48c72cf6e985b69733604ef0d309b8a9e86b345
KauhareniaNastassia <kavgorenya_a@tut.by>
2023-02-22 18:07:17 +0300

    feat(styles for navbar)

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,52 +1,50 @@
 import React from "react";
 import '../../App.css';
-import css from './Navbar.module.css'
+import css from './Navbar.module.scss'
 import {NavLink, Route} from "react-router-dom";
 import {SidebarPageContainer} from "./UsersForNavbar/SidebarPageContainer";
 
 
 export const Navbar: React.FC = () => {
     return (
-        <nav className={css.nav}>
-            <div className={css.item}>
+        <nav className={css.nav__wrapper}>
+            <div className={css.nav__item_block}>
                 <NavLink to='/profile'
-                         className={({isActive}) => isActive ? css.activeLink : ''}>
+                         className={({isActive}) => isActive ? css.nav__item_activeLink : css.nav__item}>
                     Profile
                 </NavLink>
             </div>
 
-            <div className={css.item}>
+            <div className={css.nav__item_block}>
                 <NavLink to='/dialogs'
-                         className={({isActive}) => isActive ? css.activeLink : ''}>
+                         className={({isActive}) => isActive ? css.nav__item_activeLink :  css.nav__item}>
                     Messages
                 </NavLink>
             </div>
 
-            <div className={css.item}>
+            <div className={css.nav__item_block}>
                 <NavLink to='/users'
-                         className={({isActive}) => isActive ? css.activeLink : ''}>
+                         className={({isActive}) => isActive ? css.nav__item_activeLink :  css.nav__item}>
                     Users
                 </NavLink>
             </div>
 
-            <div className={css.item}>
+            <div className={css.nav__item_block}>
                 <NavLink to='/care'
-                         className={({isActive}) => isActive ? css.activeLink : ''}>
+                         className={({isActive}) => isActive ? css.nav__item_activeLink :  css.nav__item}>
                     Care
                 </NavLink>
             </div>
 
-            <div className={css.item}>
+            <div className={css.nav__item_block}>
                 <NavLink to='/forum'
-                         className={({isActive}) => isActive ? css.activeLink : ''}>
+                         className={({isActive}) => isActive ? css.nav__item_activeLink :  css.nav__item}>
                     Forum
                 </NavLink>
             </div>
 
 
-            <div>
-                <SidebarPageContainer/>
-            </div>
+
         </nav>
     )
 }
--- src/components/Navbar/UsersForNavbar/SidebarPage.tsx
+++ src/components/Navbar/UsersForNavbar/SidebarPage.tsx
@@ -1,13 +1,31 @@
-import React from "react";
+import React, {useState} from "react";
 import css from './UsersForNavbar.module.css'
 import {SidebarUsersPropsType} from "./SidebarPageContainer";
 import {SidebarUser} from "./Friend/SidebarUser";
+import {useAppSelector} from "../../../hoc/useAppSelector";
+import {UserType} from "../../../api/usersAPI";
 
 
-export const SidebarPage = (props: SidebarUsersPropsType) => {
+export const SidebarPage:React.FC = () => {
+
+    const users = useAppSelector((state) => state.usersPage.users)
+
+    const usersForSideBar = users.filter(user => user.followed)
+
+    const [sideBarUser, setSideBarUser] = useState<UserType>()
+
+    function randomNumberInRange(min: number, max: number) {
+        //  get number between min and max
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+
+  /*  const handleClick = () => {
+        setNum(randomNumberInRange(0, 5));
+    };*/
+
     return (
         <div className={css.usersWrapper}>
-            { props.sidebarUsers.map( user => <SidebarUser key={user.id} fullName={user.name} photo={user.photos}/> ) }
+            { users.map( user => <SidebarUser key={user.id} fullName={user.name} photo={user.photos}/> )}
 
         </div>
     )
